426b909cce195b40d376b31c2654f3dc36a357d0
makarmarta <122.24makar.m@nltu.lviv.ua>
2025-10-01 09:37:48 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,60 +1,172 @@
-#include<iostream>
-#include <iomanip>
-#include<math.h>
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-double calculation_a(double x, double y, double z, double b) {
+class my_vector {
+private:
+    size_t size;
+    double* data;
 
-    double top = z + pow(sin(y), 2) + pow(y + b, 1.3);
-
-    
-    double bottom1 = (x * x + y) / (y + x * x * x / 3);
-    double bottom2 = z * z + fabs(bottom1) - pow(log(fabs(z)), 2);
-
-    return y * y + top / bottom2;
-}
-
-double calculation_b(double x, double y, double z) {
-    double part1 = (1 + pow(cos(y - 2 * x * x * x), 2));
-    double part2 = (2 + pow(fabs(x), 1.5) - pow(sin(pow(fabs(z), 0.2)), 2));
-    double part3 = pow(log(fabs(z - y)), 2);
-
-    return part1 / part2 + part3;
-}
-
-int main() {
-    int n;
-    cout << "Enter variante number:";
-    cin >> n;
-
-    double x = 0.48 * n;
-    double y = 0.47 * n;
-    double z = -1.32 * n;
-
-    double b = calculation_b(x, y, z);
-    cout << "Function value b = " << b << endl;
-
-    double a = calculation_a(x, y, z, b);
-    cout << "Function value a = " << a << endl;
-
-    cout << "   x\t\tb(x)\t\t\ta(x)" << endl;
-    cout << "----------------------------------------------------" << endl;
-
-    double start = -1;
-    double end = 1;
-    double step = 0.2;
-
-    cout << fixed << setprecision(6);
-
-    for (double x = start; x <= end; x += step) {
-        if (fabs(x) < 1e-12) x = 0;
-
-        b = calculation_b(x, y, z);
-        a = calculation_a(x, y, z, b);
-
-        cout << x << "   \t   " << b << "   \t   " << a << endl;
+public:
+    my_vector(size_t n = 0, double value = 0.0) : size(n) {
+        data = (size > 0) ? new double[size] : nullptr;
+        for (size_t i = 0; i < size; i++) data[i] = value;
     }
 
-    return 0;
+    my_vector(const my_vector& other) : size(other.size) {
+        data = (size > 0) ? new double[size] : nullptr;
+        for (size_t i = 0; i < size; i++) data[i] = other.data[i];
+    }
+
+    my_vector& operator=(const my_vector& other) {
+        if (this == &other) return *this;
+        delete[] data;
+        size = other.size;
+        data = (size > 0) ? new double[size] : nullptr;
+        for (size_t i = 0; i < size; i++) data[i] = other.data[i];
+        return *this;
+    }
+
+    ~my_vector() { delete[] data; }
+
+    size_t get_size() const { return size; }
+    void set(size_t i, double val) { if (i < size) data[i] = val; }
+    double get(size_t i) const { return (i < size) ? data[i] : 0.0; }
+
+    void show() const {
+        cout << "[ ";
+        for (size_t i = 0; i < size; i++) cout << data[i] << " ";
+        cout << "]" << endl;
+    }
+};
+
+
+class matrix {
+private:
+    size_t rows, cols;
+    my_vector* data;
+
+public:
+    matrix(size_t r = 0, size_t c = 0) : rows(r), cols(c) {
+        data = (rows > 0) ? new my_vector[rows] : nullptr;
+        for (size_t i = 0; i < rows; i++) data[i] = my_vector(cols, 0.0);
+    }
+
+    matrix(const matrix& other) : rows(other.rows), cols(other.cols) {
+        data = (rows > 0) ? new my_vector[rows] : nullptr;
+        for (size_t i = 0; i < rows; i++) data[i] = other.data[i];
+    }
+
+    matrix& operator=(const matrix& other) {
+        if (this == &other) return *this;
+        delete[] data;
+        rows = other.rows; cols = other.cols;
+        data = (rows > 0) ? new my_vector[rows] : nullptr;
+        for (size_t i = 0; i < rows; i++) data[i] = other.data[i];
+        return *this;
+    }
+
+    ~matrix() { delete[] data; }
+
+    void set(size_t i, size_t j, double val) { if (i < rows) data[i].set(j, val); }
+
+    double get(size_t i, size_t j) const { return (i < rows) ? data[i].get(j) : 0.0; }
+
+    void show() const {
+        for (size_t i = 0; i < rows; i++) data[i].show();
+    }
+
+    matrix add(const matrix& other) const {
+        matrix res(rows, cols);
+        for (size_t i = 0; i < rows; i++)
+            for (size_t j = 0; j < cols; j++)
+                res.set(i, j, get(i, j) + other.get(i, j));
+        return res;
+    }
+
+    matrix sub(const matrix& other) const {
+        matrix res(rows, cols);
+        for (size_t i = 0; i < rows; i++)
+            for (size_t j = 0; j < cols; j++)
+                res.set(i, j, get(i, j) - other.get(i, j));
+        return res;
+    }
+
+    my_vector mul(const my_vector& v) const {
+        my_vector res(rows, 0.0);
+        for (size_t i = 0; i < rows; i++) {
+            double sum = 0;
+            for (size_t j = 0; j < cols; j++) sum += get(i, j) * v.get(j);// Add the product of the matrix element to the vector element
+            res.set(i, sum);
+        }
+        return res;
+    }
+
+    matrix mul(const matrix& other) const {
+        matrix res(rows, other.cols);
+        for (size_t i = 0; i < rows; i++) {
+            for (size_t j = 0; j < other.cols; j++) {
+                double sum = 0;
+                for (size_t k = 0; k < cols; k++) sum += get(i, k) * other.get(k, j);
+                res.set(i, j, sum);
+            }
+        }
+        return res;
+    }
+};
+
+
+int main() {
+    srand(time(0)); 
+
+    size_t r1, c1, r2, c2, v_size;
+
+    cout << "Enter the dimensions of matrix A (rows and columns): ";
+    cin >> r1 >> c1;
+    matrix A(r1, c1);
+    for (size_t i = 0; i < r1; i++)
+        for (size_t j = 0; j < c1; j++)
+            A.set(i, j, rand() % 21 - 10); // -10 to 10
+
+    cout << "Enter the dimensions of matrix B (rows and columns): ";
+    cin >> r2 >> c2;
+    matrix B(r2, c2);
+    for (size_t i = 0; i < r2; i++)
+        for (size_t j = 0; j < c2; j++)
+            B.set(i, j, rand() % 21 - 10);
+
+    cout << "Enter the vector size: ";
+    cin >> v_size;
+    my_vector V(v_size);
+    for (size_t i = 0; i < v_size; i++)
+        V.set(i, rand() % 21 - 10);
+
+    cout << "\nMatrix A:" << endl; A.show();
+    cout << "Matrix B:" << endl; B.show();
+    cout << "Vector V:" << endl; V.show();
+
+    if (c1 == r2) {
+        cout << "\nA * B:" << endl;
+        A.mul(B).show();
+    }
+    else {
+        cout << "\nMultiplication A * B is impossible (the sizes do not match)." << endl;
+    }
+
+    if (c1 == V.get_size()) {
+        cout << "\nA * V:" << endl;
+        A.mul(V).show();
+    }
+    else {
+        cout << "\nMultiplication of A * V is impossible (vector size does not match)." << endl;
+    }
+
+    if (r1 == r2 && c1 == c2) {
+        cout << "\nA + B:" << endl; A.add(B).show();
+        cout << "A - B:" << endl; A.sub(B).show();
+    }
+    else {
+        cout << "\nAdding/subtracting A and B is not possible (different sizes)." << endl;
+    }
 }
